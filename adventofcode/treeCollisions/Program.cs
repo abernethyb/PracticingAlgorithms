4f8b50adc4b535cc8dd4ffab1db00335b89d0428
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2021-02-17 00:07:00 -0600

    completed sledding tree collision calculator

---
+++ adventofcode/treeCollisions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace treeCollisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input input = new Input();
+            string commaInput = input.input.Replace("\r\n", ",");
+            List<string> inputList = commaInput.Split(",").ToList();
+
+            int linePosition = 0;
+            //int listPosition = 0;
+            int treeCollisons = 0;
+
+            foreach (string line in inputList)
+            {
+
+                if (line[linePosition] == '#')
+                {
+                    //uncomment below for fun version
+                    //Console.WriteLine("OUCH!!");
+                    treeCollisons++;
+                }
+                ////uncomment below for fun version
+                // else
+                // {
+
+                //     Console.WriteLine("PHEW!");
+                // }
+
+                if (linePosition == line.Count() - 1)
+                {
+                    linePosition = 2;
+
+                }
+                else if (linePosition == line.Count() - 2)
+                {
+                    linePosition = 1;
+
+                }
+                else if (linePosition == line.Count() - 3)
+                {
+                    linePosition = 0;
+
+                }
+                else
+                {
+                    linePosition += 3;
+                }
+
+                // listPosition++;
+                // Console.WriteLine(linePosition);
+                // Console.WriteLine(listPosition);
+            }
+            Console.WriteLine($"Total number of collisions with trees: {treeCollisons}");
+
+        }
+    }
+}
