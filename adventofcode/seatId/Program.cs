234f8a10e0bc11099111ce67347f17a3fb2bf9f3
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2021-02-21 23:16:53 -0600

    completed part one. returned max seadId

--- adventofcode/seatId/Program.cs
+++ adventofcode/seatId/Program.cs
@@ -11,16 +11,16 @@ namespace seatId
             Input input = new Input { };
 
             List<string> inputList = input.input.Replace("\r\n", ",").Split(",").ToList();
-
+            List<int> seatIds = new List<int> { };
             foreach (string line in inputList)
             {
                 int[] rows = { 0, 127 };
-                //int row = -1;
-                int columns = 8;
-                int colum = -1;
+                int[] columns = { 0, 7 };
+
                 Console.WriteLine("");
-                //char[] lineChar = line.ToCharArray();
-                char[] lineChar = { 'F', 'B', 'F', 'B', 'B', 'F', 'F', 'R', 'L', 'R' };
+                char[] lineChar = line.ToCharArray();
+                //char[] lineChar = { 'F', 'B', 'F', 'B', 'B', 'F', 'F', 'R', 'L', 'R' };
+                //char[] lineChar = { 'B', 'F', 'F', 'F', 'B', 'B', 'F', 'R', 'R', 'R' };
 
                 for (int i = 0; i <= lineChar.Length - 1; i++)
                 {
@@ -40,9 +40,34 @@ namespace seatId
                         //Console.WriteLine(row);
                         Console.WriteLine($"{rows[0]}, {rows[1]}");
                     }
+                    if (lineChar[i] == 'L')
+                    {
+                        columns[1] = (columns[1] + columns[0]) / 2;
+                        //rows = rows - row;
+                        //Console.WriteLine(row);
+                        Console.WriteLine($"{columns[0]}, {columns[1]}");
+                    }
+                    else if (lineChar[i] == 'R')
+                    {
+                        columns[0] = (columns[1] + columns[0]) / 2 + 1;
+                        //rows = rows - row;
+                        //Console.WriteLine(row);
+                        Console.WriteLine($"{columns[0]}, {columns[1]}");
+                    }
                 }
+                int row = rows[0];
+                int column = columns[0];
+
+                int seatId = row * 8 + column;
+                seatIds.Add(seatId);
+
+                Console.WriteLine("");
+                Console.Write($"row: {row}, column: {column}. SeatID: {seatId}");
                 Console.WriteLine("");
             }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(seatIds.Max());
         }
     }
 }
